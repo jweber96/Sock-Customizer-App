b0116363ccd60c80db9bc2900c573e692212944d
Mike Yandam <yandam.4@osu.edu>
2020-02-08 20:06:13 -0500

    Fixed the picture sizing

--- frontend/src/cut/Cut.js
+++ frontend/src/cut/Cut.js
@@ -16,7 +16,7 @@ const cut = () => {
         <React.Fragment>
             <h1>Step 1: Select Cut Length</h1>
 
-            <Grid container direction="row" justify="space-around" alignItems="center">
+            <Grid container direction="row" justify="space-around" alignItems="center" background>
                 <Grid item sm container>
                     <Grid item container direction="column" justify="space-around" alignItems="center" spacing={2}>
                         <Grid item>
